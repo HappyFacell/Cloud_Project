d31c6e7b5e7a5f16b1e55713316c45a02eefacec
HappyFacell <is722820@iteso.mx>
2021-05-09 21:02:25 -0500

    Actualizacion 09/05/2021

--- App.js
+++ App.js
@@ -6,12 +6,13 @@ const {
     test
 } = require('./middlewares/logs')
 const loginRouter = require('./routes/Login-routes')
+const cors = require('cors')
 
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-
+app.use(cors())
 app.use(log, test);
 app.use(express.json());
 app.use('/api/user', userRouter);
