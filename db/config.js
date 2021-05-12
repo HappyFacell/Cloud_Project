72a54321ca2ee987446aefd2631167965a590b01
HappyFacell <is722820@iteso.mx>
2021-05-11 21:49:45 -0500

    Actualizacion 11/05/2021

--- db/config.js
+++ db/config.js
@@ -1,6 +1,6 @@
 let config = {
-    db_user: process.env.DB_USER,
-    password: process.env.DB_PWD,
+    db_user: 'Profesor',
+    password: 'HcaVB73NYBaxqw3h',
     db_name: 'DBProyecto',
     getUrl() {
         return `mongodb+srv://${this.db_user}:${this.password}@proyectoweb1.elums.mongodb.net/${this.db_name}?retryWrites=true&w=majority`
