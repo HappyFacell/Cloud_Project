7e1a6f84f1c148170f638a6269379d5587e4d2fa
HappyFacell <is722820@iteso.mx>
2023-04-19 22:00:44 -0600

    a ver si funciona

--- App.js
+++ App.js
@@ -1,21 +1,26 @@
 const express = require("express");
+const cors = require('cors')
 const userRouter = require("./routes/Users-routes")
 const matrixRouter = require("./routes/Matrix-routes")
+const loginRouter = require('./routes/Login-routes')
 const {
     log,
     test
 } = require('./middlewares/logs')
-const loginRouter = require('./routes/Login-routes')
-const cors = require('cors')
 
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+
 app.use(cors())
-app.use(log, test);
-app.use(express.static(__dirname + "/public"))
 app.use(express.json());
+app.use(log, test);
+
+
+app.use(express.static(__dirname + "/public"))
+
+
 app.use('/api/user', userRouter);
 app.use('/api/matrix', matrixRouter);
 app.use('/api/auth', loginRouter);
--- db/config.js
+++ db/config.js
@@ -1,9 +1,10 @@
 let config = {
     db_user: process.env.DB_USER,
     password: process.env.DB_PWD,
-    db_name: 'DBProyecto',
+    // db_name: 'DBProyecto',
     getUrl() {
-        return `mongodb+srv://${this.db_user}:${this.password}@proyectoweb1.elums.mongodb.net/${this.db_name}?retryWrites=true&w=majority`
+        // return `mongodb+srv://${this.db_user}:${this.password}@proyectoweb1.elums.mongodb.net/${this.db_name}?retryWrites=true&w=majority`
+        return `mongodb+srv://diego_ferreira:${this.password}@cloudproject.w5br33t.mongodb.net/${this.db_name}?retryWrites=true&w=majority`
     }
 }
 
